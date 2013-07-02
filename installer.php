135ca633cb9073fb9512243cb77694cc238df66e
shannah <steve@weblite.ca>
2013-07-02 11:16:20 -0700

    Added translation_memory_uuid column so that we can uniquely specify translation memories across instances.  It is possible, then to synchronize translations memories across distributed systems.

--- installer.php
+++ installer.php
@@ -453,6 +453,14 @@ class modules_tm_installer {
 	
 	}
 	
+        public function update_11(){
+	    $sql[] = "ALTER TABLE `xf_tm_translation_memories` ADD COLUMN `translation_memory_uuid` CHAR(36) NULL  AFTER `translation_memory_id`, ADD UNIQUE INDEX `translation_memory_uuid_UNIQUE` (`translation_memory_uuid` ASC)";
+	    $sql[] = "update xf_tm_translation_memories set translation_memory_uuid=UUID()";
+	    df_q($sql);
+	    df_clear_views();
+	    
+	}
+        
 	public static function query($sql){
 		if ( is_array($sql) ){
 			$res = null;
--- tables/xf_tm_translation_memories/xf_tm_translation_memories.php
+++ tables/xf_tm_translation_memories/xf_tm_translation_memories.php
@@ -6,4 +6,19 @@ class tables_xf_tm_translation_memories {
 		$tmCopy = new XFTranslationMemory($copy);
 		$tmCopy->import($tmOriginal);
 	}
+	
+	function afterInsert(Dataface_Record $tm){
+	    df_q(sprintf(
+	        "update xf_tm_translation_memories set translation_memory_uuid=UUID() where translation_memory_id=%d",
+	        $tm->val('translation_memory_id')
+	    ));
+	    $res = df_q(sprintf(
+	        "select translation_memory_uuid from xf_tm_translation_memories where translation_memory_id=%d",
+	        $tm->val('translation_memory_id')
+	    ));
+	    list($uuid) = mysql_fetch_row($res);
+	    @mysql_free_result($res);
+	    $tm->setValue('translation_memory_uuid', $uuid);
+	    
+	}
 }
